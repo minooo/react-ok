874399bb0ff8d65e7ab1de68fbbf03594d67d6e2
minooo <x13603983223@163.com>
2019-04-17 07:07:39 +0800

    done2

--- src/components/movie/MovieDetail.jsx
+++ src/components/movie/MovieDetail.jsx
@@ -1,43 +1,45 @@
-import React, { Component } from 'react'
-import { Spin, Button, Icon } from 'antd'
-import fetchJsonp from 'fetch-jsonp'
+import React, { Component } from 'react';
+import { Spin, Button, Icon } from 'antd';
+import fetchJsonp from 'fetch-jsonp';
 
 export default class MovieDetail extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            movieinfo: {},
-            isLoading: true
-        }
-    }
+		this.state = {
+			movieinfo: {},
+			isLoading: true
+		};
+	}
 
-    render() {
-        return (
-            <div>
-                <Button type="primary" onClick={() => this.props.history.go(-1)}>
-                    <Icon type="left" />后退
-                </Button>
-                {
-                    this.state.isLoading ?
-                        <div style={{ textAlign: 'center' }}><Spin size="large" /></div> :
-                        <div style={{ textAlign: 'center' }}>
-                            <h1 style={{ fontSize: '35px', fontWeight: 'bold' }}>{this.state.movieinfo.title}</h1>
-                            <img src={this.state.movieinfo.images.large} alt="" />
-                            <p style={{ lineHeight: '30px', textIndent: '2em' }}>{this.state.movieinfo.summary}</p>
-                        </div>
-                }
-            </div>
-        )
-    }
-    componentDidMount() {
-        console.log(this.props)
-        this.getMovieInfo()
-    }
+	render() {
+		return (
+			<div>
+				<Button type="primary" onClick={() => this.props.history.go(-1)}>
+					<Icon type="left" />后退
+				</Button>
+				{this.state.isLoading ? (
+					<div style={{ textAlign: 'center' }}>
+						<Spin size="large" />
+					</div>
+				) : (
+					<div style={{ textAlign: 'center' }}>
+						<h1 style={{ fontSize: '35px', fontWeight: 'bold' }}>{this.state.movieinfo.title}</h1>
+						<img src={this.state.movieinfo.images.large} alt="" />
+						<p style={{ lineHeight: '30px', textIndent: '2em' }}>{this.state.movieinfo.summary}</p>
+					</div>
+				)}
+			</div>
+		);
+	}
+	componentDidMount() {
+		console.log(this.props);
+		this.getMovieInfo();
+	}
 
-    getMovieInfo = async () => {
-        const response = await fetchJsonp(`https://api.douban.com/v2/movie/subject/${this.props.match.params.id}`)
-        const data = await response.json()
-        this.setState({ movieinfo: data, isLoading: false })
-    }
+	getMovieInfo = async () => {
+		const response = await fetchJsonp(`https://api.douban.com/v2/movie/subject/${this.props.match.params.id}`);
+		const data = await response.json();
+		this.setState({ movieinfo: data, isLoading: false });
+	};
 }
